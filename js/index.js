11f9d8236bd3ea26ab7aec6409f9674d284686ad
jesszin <jarisnotbowl@gmail.com>
2018-01-17 10:38:16 +0900

    index

---
+++ js/index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+    $('#navicon').click(function(){
+        $(this).toggleClass('open');
+        $('.navWrap').slideToggle();
+    }); //menu slide jquery
+
+});//end
